90c94a894f9c2efeb7845ed6136bdf71bd261d81
ilhae Ye <yeaih89@gmail.com>
2016-08-29 00:55:09 +0900

    Test

---
+++ Test/Test/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int size = 100000;
+
+	vector<int> v(size);
+
+	clock_t begin_time = clock();
+	for (int i = 0; i < size; i++)
+	{
+		v[i] = 0;
+	}
+	float test1 = float(clock() - begin_time) / CLOCKS_PER_SEC;
+
+
+	vector<int> v2(size);	
+	clock_t begin_time2 = clock();
+	for (vector<int>::iterator it = v2.begin(); it != v2.end(); it++)
+	{
+		(*it) = 0;
+	}
+	float test2 = float(clock() - begin_time) / CLOCKS_PER_SEC;
+
+	cout << "test1 :: " << test1 << endl;
+	cout << "test2 :: " << test2 << endl;
+
+	return 0;
+}
+
+
+// size 10000 
+// test 1 :: 0.004
+// test 2 :: 0.029
+
+// size 100000
+// test 1 :: 0.039
+// test 2 :: 0.31
+
+// vector는 랜덤 접근에 최적화 되어 있으므로,
+// iterator 보다는 index가 속도가 빠르다.
